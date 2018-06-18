b6a4968176e37973ce4a1114ef07519e83a55db2
Emma Johansson Vikström <emmakristina95@hotmail.com>
2018-06-18 17:51:40 +0200

    show all games

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductsController extends BaseController
 {
   public function showProducts() {
-
+    $results = DB::select('select * from products');
+    return $results;
+    //return view('games', ['result' => $results]);
   }
 
   public function showOneProduct() {
@@ -15,6 +22,6 @@ class ProductsController extends BaseController
   }
 
   public function createProduct() {
-    
+
   }
 }
--- app/Http/Controllers/StoresController.php
+++ app/Http/Controllers/StoresController.php
@@ -7,6 +7,6 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 class StoresController extends BaseController
 {
   public function showStores() {
-
+    
   }
 }
