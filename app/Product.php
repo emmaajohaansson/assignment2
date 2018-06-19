384ad5aaffd84c5d9e0e5aa0e52121a3063099ef
Emma Johansson Vikström <emmakristina95@hotmail.com>
2018-06-19 14:53:56 +0200

    adding models and relationships

---
+++ app/Product.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    public function reviews(){
+      return $this->hasMany('App\Review');
+    }
+
+    public function stores(){
+      return $this->hasMany('App\Store');
+    }
+}
